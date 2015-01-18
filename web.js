fda5a78ee391efc0d3538d5ceac94722e72d6e95
Tim Sherratt <tim@discontents.com.au>
2015-01-18 22:21:25 +1100

    Comment out logging

--- web.js
+++ web.js
@@ -2,6 +2,6 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
  
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
 app.use(gzippo.staticGzip('' + __dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
